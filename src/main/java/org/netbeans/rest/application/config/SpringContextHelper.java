636fd37e99f651d157bc2cba367ce9ac6dc3b5d1
csarmiento <tactusoft@hotmail.com>
2015-09-29 17:04:25 -0500

    Funcionalidad Clientes

---
+++ src/main/java/org/netbeans/rest/application/config/SpringContextHelper.java
@@ -0,0 +1,18 @@
+package org.netbeans.rest.application.config;
+
+import javax.servlet.ServletContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+public class SpringContextHelper {
+
+	private ApplicationContext context;
+
+	public SpringContextHelper(ServletContext servletContext) {
+		context = WebApplicationContextUtils.getRequiredWebApplicationContext(servletContext);
+	}
+
+	public Object getBean(final String beanRef) {
+		return context.getBean(beanRef);
+	}
+}
